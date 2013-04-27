b8712a3da9f958911dfd219659e2664a24739919
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-04-26 21:16:33 -0500

    Implement get/put for Currency

--- Amount.hs
+++ Amount.hs
@@ -1,9 +1,13 @@
 module Amount where
 
+import Control.Monad
+import Control.Applicative
 import Data.Bits
 import Data.Word
-import Data.Binary (Binary(..), Get)
+import Data.Binary (Binary(..), Get, putWord8)
+import Data.Binary.Get (getLazyByteString)
 import Data.Base58Address (RippleAddress)
+import qualified Data.ByteString.Lazy as LZ
 
 data Currency = XRP | Currency (Char,Char,Char) RippleAddress
 	deriving (Eq)
@@ -11,6 +15,32 @@ data Currency = XRP | Currency (Char,Char,Char) RippleAddress
 instance Show Currency where
 	show XRP = "XRP"
 	show (Currency (a,b,c) adr) = [a,b,c,'/'] ++ show adr
+
+instance Binary Currency where
+	get = do
+		allZero <- getLazyByteString 12
+		currency <- getLazyByteString 3
+		version <- getLazyByteString 2
+		reserved <- getLazyByteString 3
+		issuer <- get
+
+		when (LZ.any (/=0) allZero) (fail "Bad currency format az")
+		when (LZ.any (/=0) version) (fail "Bad currency format ver")
+		when (LZ.any (/=0) reserved) (fail "Bad currency format res")
+
+		-- Spec says ASCII
+		let [a,b,c] = map (toEnum.fromIntegral) $ LZ.unpack currency
+		return $ Currency (a,b,c) issuer
+
+	put XRP = fail "XRP does not get encoded as a currency specifier."
+	put (Currency (a,b,c) issuer) = do
+		replicateM_ 12 (putWord8 0)
+		putWord8 $ fromIntegral $ fromEnum a
+		putWord8 $ fromIntegral $ fromEnum b
+		putWord8 $ fromIntegral $ fromEnum c
+		replicateM_ 2 (putWord8 0)
+		replicateM_ 3 (putWord8 0)
+		put issuer
 
 data Amount = Amount Rational Currency
 	deriving (Eq)
