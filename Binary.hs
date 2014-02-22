945ef9c9a6676d898de295c128c59787b44fe032
Stephen Paul Weber <singpolyma@singpolyma.net>
2014-02-22 15:21:44 -0500

    Handle unknown fields

--- Binary.hs
+++ Binary.hs
@@ -5,8 +5,9 @@ import Control.Monad
 import Control.Applicative
 import Data.List
 import Data.Word
+import Data.LargeWord
 import Data.Bits
-import Data.Binary (Binary(..), Get, putWord8, getWord8, encode)
+import Data.Binary (Binary(..), Put, Get, putWord8, getWord8, encode)
 import Data.Binary.Get (isEmpty, getLazyByteString)
 import Data.Binary.Put (putLazyByteString)
 import Data.Bool.HT (select)
@@ -53,6 +54,51 @@ instance Binary VariableLengthData where
 		l2 = l - 193
 		l = LZ.length bytes
 
+instance (Binary a, Binary b) => Binary (LargeKey a b) where
+	put (LargeKey lo hi) = put hi >> put lo
+	get = flip LargeKey <$> get <*> get
+
+data TypedField =
+	TF1  Word16             |
+	TF2  Word32             |
+	TF3  Word64             |
+	TF4  Word128            |
+	TF5  Word256            |
+	TF6  Amount             |
+	TF7  VariableLengthData |
+	TF8  RippleAddress      |
+	TF14 [TypedField]       |
+	TF15 [[TypedField]]     |
+	TF16 Word8              |
+	TF17 Word160            |
+	TF19 [Word256]
+	deriving (Show, Eq)
+
+putTF :: TypedField -> (Word8, Put)
+putTF (TF1  x) = (01, put x)
+putTF (TF2  x) = (02, put x)
+putTF (TF3  x) = (03, put x)
+putTF (TF4  x) = (04, put x)
+putTF (TF5  x) = (05, put x)
+putTF (TF6  x) = (05, put x)
+putTF (TF7  x) = (07, put x)
+putTF (TF8  x) = (08, putWord8 20 >> put x)
+putTF (TF16 x) = (16, put x)
+putTF (TF17 x) = (17, put x)
+putTF (TF19 x) = (19, put $ VariableLengthData $ LZ.concat (map encode x))
+
+getTF :: Word8 -> Get TypedField
+getTF 01 = TF1  <$> get
+getTF 02 = TF2  <$> get
+getTF 03 = TF3  <$> get
+getTF 04 = TF4  <$> get
+getTF 05 = TF5  <$> get
+getTF 06 = TF6  <$> get
+getTF 07 = TF7  <$> get
+getTF 08 = TF8  <$> getVariableRippleAddress
+getTF 16 = TF16 <$> get
+getTF 17 = TF17 <$> get
+
 data Field =
 	LedgerEntryType Word16          |
 	TransactionType Word16          |
@@ -95,7 +141,8 @@ data Field =
 	Target RippleAddress            |
 	AuthorizedKey RippleAddress     |
 	TemplateEntryType Word8         |
-	TransactionResult Word8
+	TransactionResult Word8         |
+	UnknownField Word8 TypedField
 	deriving (Show, Eq)
 
 instance Ord Field where
@@ -110,95 +157,97 @@ instance Binary Field where
 		fld <- case tag .&. 0x0F of
 			0 -> get
 			t -> return t
-		getField (typ,fld)
+		tf <- getTF typ
+		return $ getField fld tf
 
-	put (LedgerEntryType x) = mapM_ put (packTypFld (01, 01)) >> put x
-	put (TransactionType x) = mapM_ put (packTypFld (01, 02)) >> put x
-	put (Flags x) = mapM_ put (packTypFld (02, 02)) >> put x
-	put (SourceTag x) = mapM_ put (packTypFld (02, 03)) >> put x
-	put (SequenceNumber x) = mapM_ put (packTypFld (02, 04)) >> put x
-	put (PreviousTransactionLedgerSequence x) = mapM_ put (packTypFld (02, 05)) >> put x
-	put (LedgerSequence x) = mapM_ put (packTypFld (02, 06)) >> put x
-	put (LedgerCloseTime x) = mapM_ put (packTypFld (02, 07)) >> put x
-	put (ParentLedgerCloseTime x) = mapM_ put (packTypFld (02, 08)) >> put x
-	put (SigningTime x) = mapM_ put (packTypFld (02, 09)) >> put x
-	put (ExpirationTime x) = mapM_ put (packTypFld (02, 10)) >> put x
-	put (TransferRate x) = mapM_ put (packTypFld (02, 11)) >> put x
-	put (WalletSize x) = mapM_ put (packTypFld (02, 12)) >> put x
-	put (Binary.Amount x) = mapM_ put (packTypFld (06, 01)) >> put x
-	put (Balance x) = mapM_ put (packTypFld (06, 02)) >> put x
-	put (Limit x) = mapM_ put (packTypFld (06, 03)) >> put x
-	put (TakerPays x) = mapM_ put (packTypFld (06, 04)) >> put x
-	put (TakerGets x) = mapM_ put (packTypFld (06, 05)) >> put x
-	put (LowLimit x) = mapM_ put (packTypFld (06, 06)) >> put x
-	put (HighLimit x) = mapM_ put (packTypFld (06, 07)) >> put x
-	put (Fee x) = mapM_ put (packTypFld (06, 08)) >> put x
-	put (SendMaximum x) = mapM_ put (packTypFld (06, 09)) >> put x
-	put (PublicKey x) = mapM_ put (packTypFld (07, 01)) >> put x
-	put (MessageKey x) = mapM_ put (packTypFld (07, 02)) >> put x
-	put (SigningPublicKey x) = mapM_ put (packTypFld (07, 03)) >> put x
-	put (TransactionSignature x) = mapM_ put (packTypFld (07, 04)) >> put x
-	put (Generator x) = mapM_ put (packTypFld (07, 05)) >> put x
-	put (Signature x) = mapM_ put (packTypFld (07, 06)) >> put x
-	put (Domain x) = mapM_ put (packTypFld (07, 07)) >> put x
-	put (FundScript x) = mapM_ put (packTypFld (07, 08)) >> put x
-	put (RemoveScript x) = mapM_ put (packTypFld (07, 09)) >> put x
-	put (ExpireScript x) = mapM_ put (packTypFld (07, 10)) >> put x
-	put (CreateScript x) = mapM_ put (packTypFld (07, 11)) >> put x
-	put (Account x) = mapM_ put (packTypFld (08, 01)) >> putWord8 20 >> put x
-	put (Owner x) = mapM_ put (packTypFld (08, 02)) >> putWord8 20 >> put x
-	put (Destination x) = mapM_ put (packTypFld (08, 03)) >> putWord8 20 >> put x
-	put (Issuer x) = mapM_ put (packTypFld (08, 04)) >> putWord8 20 >> put x
-	put (Target x) = mapM_ put (packTypFld (08, 05)) >> putWord8 20 >> put x
-	put (AuthorizedKey x) = mapM_ put (packTypFld (08, 06)) >> putWord8 20 >> put x
-	put (LedgerCloseTimeResolution x) = mapM_ put (packTypFld (16, 01)) >> put x
-	put (TemplateEntryType x) = mapM_ put (packTypFld (16, 02)) >> put x
-	put (TransactionResult x) = mapM_ put (packTypFld (16, 03)) >> put x
+	put (LedgerEntryType x) = putTaggedTF 01 $ TF1 x
+	put (TransactionType x) = putTaggedTF 02 $ TF1 x
+	put (Flags x) = putTaggedTF 02 $ TF2 x
+	put (SourceTag x) = putTaggedTF 03 $ TF2 x
+	put (SequenceNumber x) = putTaggedTF 04 $ TF2 x
+	put (PreviousTransactionLedgerSequence x) = putTaggedTF 05 $ TF2 x
+	put (LedgerSequence x) = putTaggedTF 06 $ TF2 x
+	put (LedgerCloseTime x) = putTaggedTF 07 $ TF2 x
+	put (ParentLedgerCloseTime x) = putTaggedTF 08 $ TF2 x
+	put (SigningTime x) = putTaggedTF 09 $ TF2 x
+	put (ExpirationTime x) = putTaggedTF 10 $ TF2 x
+	put (TransferRate x) = putTaggedTF 11 $ TF2 x
+	put (WalletSize x) = putTaggedTF 12 $ TF2 x
+	put (Binary.Amount x) = putTaggedTF 01 $ TF6 x
+	put (Balance x) = putTaggedTF 02 $ TF6 x
+	put (Limit x) = putTaggedTF 03 $ TF6 x
+	put (TakerPays x) = putTaggedTF 04 $ TF6 x
+	put (TakerGets x) = putTaggedTF 05 $ TF6 x
+	put (LowLimit x) = putTaggedTF 06 $ TF6 x
+	put (HighLimit x) = putTaggedTF 07 $ TF6 x
+	put (Fee x) = putTaggedTF 08 $ TF6 x
+	put (SendMaximum x) = putTaggedTF 09 $ TF6 x
+	put (PublicKey x) = putTaggedTF 01 $ TF7 x
+	put (MessageKey x) = putTaggedTF 02 $ TF7 x
+	put (SigningPublicKey x) = putTaggedTF 03 $ TF7 x
+	put (TransactionSignature x) = putTaggedTF 04 $ TF7 x
+	put (Generator x) = putTaggedTF 05 $ TF7 x
+	put (Signature x) = putTaggedTF 06 $ TF7 x
+	put (Domain x) = putTaggedTF 07 $ TF7 x
+	put (FundScript x) = putTaggedTF 08 $ TF7 x
+	put (RemoveScript x) = putTaggedTF 09 $ TF7 x
+	put (ExpireScript x) = putTaggedTF 10 $ TF7 x
+	put (CreateScript x) = putTaggedTF 11 $ TF7 x
+	put (Account x) = putTaggedTF 01 $ TF8 x
+	put (Owner x) = putTaggedTF 02 $ TF8 x
+	put (Destination x) = putTaggedTF 03 $ TF8 x
+	put (Issuer x) = putTaggedTF 04 $ TF8 x
+	put (Target x) = putTaggedTF 05 $ TF8 x
+	put (AuthorizedKey x) = putTaggedTF 06 $ TF8 x
+	put (LedgerCloseTimeResolution x) = putTaggedTF 01 $ TF16 x
+	put (TemplateEntryType x) = putTaggedTF 02 $ TF16 x
+	put (TransactionResult x) = putTaggedTF 03 $ TF16 x
+	put (UnknownField tag tf) = putTaggedTF tag tf
 
-getField :: (Word8,Word8) -> Get Field
-getField (01,01) = LedgerEntryType <$> get
-getField (01,02) = TransactionType <$> get
-getField (02,02) = Flags <$> get
-getField (02,03) = SourceTag <$> get
-getField (02,04) = SequenceNumber <$> get
-getField (02,05) = PreviousTransactionLedgerSequence <$> get
-getField (02,06) = LedgerSequence <$> get
-getField (02,07) = LedgerCloseTime <$> get
-getField (02,08) = ParentLedgerCloseTime <$> get
-getField (02,09) = SigningTime <$> get
-getField (02,10) = ExpirationTime <$> get
-getField (02,11) = TransferRate <$> get
-getField (02,12) = WalletSize <$> get
-getField (06,01) = Binary.Amount <$> get
-getField (06,02) = Balance <$> get
-getField (06,03) = Limit <$> get
-getField (06,04) = TakerPays <$> get
-getField (06,05) = TakerGets <$> get
-getField (06,06) = LowLimit <$> get
-getField (06,07) = HighLimit <$> get
-getField (06,08) = Fee <$> get
-getField (06,09) = SendMaximum <$> get
-getField (07,01) = PublicKey <$> get
-getField (07,02) = MessageKey <$> get
-getField (07,03) = SigningPublicKey <$> get
-getField (07,04) = TransactionSignature <$> get
-getField (07,05) = Generator <$> get
-getField (07,06) = Signature <$> get
-getField (07,07) = Domain <$> get
-getField (07,08) = FundScript <$> get
-getField (07,09) = RemoveScript <$> get
-getField (07,10) = ExpireScript <$> get
-getField (07,11) = CreateScript <$> get
-getField (08,01) = Account <$> getVariableRippleAddress
-getField (08,02) = Owner <$> getVariableRippleAddress
-getField (08,03) = Destination <$> getVariableRippleAddress
-getField (08,04) = Issuer <$> getVariableRippleAddress
-getField (08,05) = Target <$> getVariableRippleAddress
-getField (08,06) = AuthorizedKey <$> getVariableRippleAddress
-getField (16,01) = LedgerCloseTimeResolution <$> get
-getField (16,02) = TemplateEntryType <$> get
-getField (16,03) = TransactionResult <$> get
-getField x = fail $ "Unknown Ripple field: " ++ show x
+getField :: Word8 -> TypedField -> Field
+getField 01 (TF1  x) = LedgerEntryType x
+getField 02 (TF1  x) = TransactionType x
+getField 02 (TF2  x) = Flags x
+getField 03 (TF2  x) = SourceTag x
+getField 04 (TF2  x) = SequenceNumber x
+getField 05 (TF2  x) = PreviousTransactionLedgerSequence x
+getField 06 (TF2  x) = LedgerSequence x
+getField 07 (TF2  x) = LedgerCloseTime x
+getField 08 (TF2  x) = ParentLedgerCloseTime x
+getField 09 (TF2  x) = SigningTime x
+getField 10 (TF2  x) = ExpirationTime x
+getField 11 (TF2  x) = TransferRate x
+getField 12 (TF2  x) = WalletSize x
+getField 01 (TF6  x) = Binary.Amount x
+getField 02 (TF6  x) = Balance x
+getField 03 (TF6  x) = Limit x
+getField 04 (TF6  x) = TakerPays x
+getField 05 (TF6  x) = TakerGets x
+getField 06 (TF6  x) = LowLimit x
+getField 07 (TF6  x) = HighLimit x
+getField 08 (TF6  x) = Fee x
+getField 09 (TF6  x) = SendMaximum x
+getField 01 (TF7  x) = PublicKey x
+getField 02 (TF7  x) = MessageKey x
+getField 03 (TF7  x) = SigningPublicKey x
+getField 04 (TF7  x) = TransactionSignature x
+getField 05 (TF7  x) = Generator x
+getField 06 (TF7  x) = Signature x
+getField 07 (TF7  x) = Domain x
+getField 08 (TF7  x) = FundScript x
+getField 09 (TF7  x) = RemoveScript x
+getField 10 (TF7  x) = ExpireScript x
+getField 11 (TF7  x) = CreateScript x
+getField 01 (TF8  x) = Account x
+getField 02 (TF8  x) = Owner x
+getField 03 (TF8  x) = Destination x
+getField 04 (TF8  x) = Issuer x
+getField 05 (TF8  x) = Target x
+getField 06 (TF8  x) = AuthorizedKey x
+getField 01 (TF16 x) = LedgerCloseTimeResolution x
+getField 02 (TF16 x) = TemplateEntryType x
+getField 03 (TF16 x) = TransactionResult x
+getField tag tf      = UnknownField tag tf
 
 -- For weird encoding of address that also includes length
 getVariableRippleAddress :: Get RippleAddress
@@ -208,12 +257,15 @@ getVariableRippleAddress = do
 		fail $ "RippleAddress is 160 bit encoding, len is " ++ show len
 	get
 
-packTypFld :: (Word8,Word8) -> [Word8]
-packTypFld (x,y)
-	| x < 16 && y < 16 = [(x `shiftL` 4) .|. y]
-	| x < 16 = [x `shiftL` 4, y]
-	| y < 16 = [y, x]
-	| otherwise = [0, x, y]
+putTaggedTF :: Word8 -> TypedField -> Put
+putTaggedTF tag tf = mapM_ put header >> fld
+	where
+	header
+		| typ < 16 && tag < 16 = [(typ `shiftL` 4) .|. tag]
+		| typ < 16 = [typ `shiftL` 4, tag]
+		| tag < 16 = [tag, typ]
+		| otherwise = [0, typ, tag]
+	(typ, fld) = putTF tf
 
 newtype Transaction = Transaction [Field]
 	deriving (Show, Eq)
